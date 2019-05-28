f23ac2b343f565b23e87e80d73807674981f6a6c
vasiliy-romanov <47746406+vasiliy-romanov@users.noreply.github.com>
2019-05-28 11:02:44 +0400

    Update NumberService.php

--- src/Rabbit/NumberService.php
+++ src/Rabbit/NumberService.php
@@ -36,25 +36,15 @@ class NumberService implements ConsumerInterface
         $response = json_decode($body, true);
         $result = $this->manager->find(FactorialResult::class, $response['id']);
 
-//        var_dump($this->repository);
-
         $number = $response['number'];
-
-//        var_dump($number);
         $rez = 1;
 
 
         for ($i = 2; $i <= $number; ++$i){
             $rez = bcmul($rez, $i);
-//          var_dump($i);
         }
 
-
-//        var_dump($rez);
-
         $result->setRezult($rez);
-
-//      var_dump($result);
 
         $this->manager->persist($result);
         $this->manager->flush();
